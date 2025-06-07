23e28268e80308bf1a708d2a497f9c05315dcd2a
mhmadjh89 <mhmadjh89@gmail.com>
2025-06-07 16:39:55 -0400

    message soraka 2

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,26 +3,28 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+// Icons
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
 import EditIcon from '@mui/icons-material/Edit';
 
-export default function Todo(){
+export default function Todo({title, details}:{title: string, details: string}){
     return(
     <React.Fragment>
         <Card className="todoCard" sx={{ minWidth: 275 , background:"#123456", color: "white", marginTop:"15px"}}>
         <CardContent style={{textAlign: "center"}}>
         <Grid container spacing={2}>
+           <Grid size={8}>
+            <Typography variant="h5" sx={{textAlign: "left"}}>{title}</Typography>
+            <Typography variant="h6" sx={{textAlign: "left"}}>{details}</Typography>
+           </Grid>
            <Grid size={4} display="flex" justifyContent="space-around" alignItems="center"> 
            <IconButton className="iconButton" aria-label="delete" style={{color:"#b23c17", background: "white", border: "solid #b23c17 3px"}}><DeleteIcon /></IconButton>
            <IconButton className="iconButton" aria-label="edit"  style={{color:"#1769aa", background: "white", border: "solid #1769aa 3px"}}><EditIcon /></IconButton>
            <IconButton className="iconButton" aria-label="check"  style={{color:"#8bc34a", background: "white", border: "solid #8bc34a 3px"}}><CheckIcon /></IconButton>
            </Grid>
-           <Grid size={8}>
-            <Typography variant="h5" sx={{textAlign: "right"}}>Task 1</Typography>
-            <Typography variant="h6" sx={{textAlign: "right"}}>Task 1</Typography>
-           </Grid>
+           
         </Grid>
       
         </CardContent>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,11 +5,46 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 // Components
 import Todo from "./Todo"
 
+// Others
+import {v4 as uuidv4} from 'uuid'
+
+const todos:{
+  id: string,
+  title: string,
+  details: string,
+  isCompleted: boolean
+}[] = [
+  {
+    id: uuidv4(),
+    title: "Read a Book",
+  details: "One Book",
+  isCompleted: false
+  },
+  {
+    id: uuidv4(),
+    title: "Read a Book",
+    details: "One Book",
+    isCompleted: false
+  },
+  {
+      id: uuidv4(),
+      title: "Read a Book",
+      details: "One Book",
+      isCompleted: false
+  }
+]
+
 export default function TodoList() {
+  const uitodo = todos.map((t)=>{
+    return <Todo key={t.id} title={t.title} details={t.details}/>
+  })
   return (
     <Container maxWidth="sm">
       <Card sx={{ minWidth: 275}}>
@@ -31,7 +66,11 @@ export default function TodoList() {
         <ToggleButton value="wait">NDone</ToggleButton>
       </ToggleButtonGroup>
       {/* All todo */}
-      <Todo />
+      {uitodo}
+      <Grid container spacing={2} style={{marginTop: "20px"}}>
+           <Grid size={4}><Button style={{width: "100%", height: "100%", backgroundColor: "#123456"}}variant="contained">Add</Button></Grid>
+           <Grid size={8}><TextField style={{width: "100%"}} id="outlined-basic" label="Name Task" variant="outlined" /></Grid>
+      </Grid>
       </CardContent>
     </Card>
     </Container>
