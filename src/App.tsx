4f3afb29f42447882b609758e80096d047ce649d
mhmadjh89 <mhmadjh89@gmail.com>
2025-06-09 15:01:40 -0400

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import './App.css'
 import TodoList from './components/TodoList'
+import { TodosContext } from './contexts/todosContext'
+import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+
+type User = {
+  id: string,
+  title: string,
+  details: string,
+  isCompleted: boolean
+}
+const MainTodos:User[] = [
+  {
+    id: uuidv4(),
+    title: "Read a Book",
+  details: "One Book",
+  isCompleted: false
+  },
+  {
+    id: uuidv4(),
+    title: "Read a Book",
+    details: "One Book",
+    isCompleted: false
+  },
+  {
+      id: uuidv4(),
+      title: "Read a Book",
+      details: "One Book",
+      isCompleted: false
+  }
+]
 
 function App() {
+  const [todos, setTodos] = useState(MainTodos);
+
   return (
     <div className="app" style={{display: "flex",justifyContent: "center", alignItems: "center", backgroundColor: "#191b1f", height: "100vh"}}>
-      <TodoList/>
+      <TodosContext.Provider value={{todos, setTodos}}>
+          <TodoList/>
+      </TodosContext.Provider >
     </div>
   )
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,26 +3,113 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import { useContext, useState } from 'react';
+import { TodosContext } from '../contexts/todosContext';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Button, TextField } from '@mui/material';
+
 // Icons
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
 import EditIcon from '@mui/icons-material/Edit';
 
-export default function Todo({title, details}:{title: string, details: string}){
+
+export default function Todo({todo}:any){
+    const [showDeleteDialog, setShowDeleteDialog]: any = useState(false)
+    const [showUpdateDialog, setShowUpdateDialog]: any = useState(false)
+    const {todos, setTodos }: any = useContext(TodosContext)
+
+    function handleCheckClick() {
+        const updateTodos = todos.map((t: { id: string; isCompleted: boolean; }) => {
+            if (t.id == todo.id) {
+              t.isCompleted = !t.isCompleted;
+            }
+            return t; 
+        });
+        setTodos(updateTodos);
+        localStorage.setItem("todos", JSON.stringify(updateTodos));
+    }
+
+    function handleDeleteClick() {
+        setShowDeleteDialog(true)
+    }
+
+    function handleCloseDelete() {
+        setShowDeleteDialog(false)
+    }
+    function handleCloseUpdate() {
+        setShowUpdateDialog(false)
+    }
+    function handleDeleteConfirm(){
+        const updateTodos = todos.filter((t: { id: string; isCompleted: boolean; }) => {
+            return t.id != todo.id
+        });
+        setTodos(updateTodos);
+    }
+    function handleUpdateConfirm(){
+        const updateTodos = todos.filter((t: { id: string; isCompleted: boolean; }) => {
+            return t.id != todo.id
+        });
+        setTodos(updateTodos);
+    }
     return(
     <React.Fragment>
+        <Dialog
+            open={showDeleteDialog}
+            onClose={handleCloseDelete}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+            {"Are you sure about delete this task ?"}
+            </DialogTitle>
+            <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+                You cannot back after remove
+            </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleCloseDelete}>Cancle</Button>
+            <Button onClick={handleDeleteConfirm}autoFocus>
+                Agree
+            </Button>
+            </DialogActions>
+        </Dialog>
+        <Dialog
+            open={showUpdateDialog}
+            onClose={handleCloseUpdate}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+            {"Are you sure about delete this task ?"}
+            </DialogTitle>
+            <DialogContent>
+                <TextField autoFocus margin="dense" id="name" label="Edit Task" fullWidth variant='standard'/>
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={handleCloseUpdate}>Cancle</Button>
+            <Button onClick={handleUpdateConfirm}autoFocus>
+                Done
+            </Button>
+            </DialogActions>
+        </Dialog>
         <Card className="todoCard" sx={{ minWidth: 275 , background:"#123456", color: "white", marginTop:"15px"}}>
         <CardContent style={{textAlign: "center"}}>
         <Grid container spacing={2}>
            <Grid size={8}>
-            <Typography variant="h5" sx={{textAlign: "left"}}>{title}</Typography>
-            <Typography variant="h6" sx={{textAlign: "left"}}>{details}</Typography>
+            <Typography variant="h5" sx={{textAlign: "left"}}>{todo.title}</Typography>
+            <Typography variant="h6" sx={{textAlign: "left"}}>{todo.details}</Typography>
            </Grid>
            <Grid size={4} display="flex" justifyContent="space-around" alignItems="center"> 
            <IconButton className="iconButton" aria-label="delete" style={{color:"#b23c17", background: "white", border: "solid #b23c17 3px"}}><DeleteIcon /></IconButton>
-           <IconButton className="iconButton" aria-label="edit"  style={{color:"#1769aa", background: "white", border: "solid #1769aa 3px"}}><EditIcon /></IconButton>
-           <IconButton className="iconButton" aria-label="check"  style={{color:"#8bc34a", background: "white", border: "solid #8bc34a 3px"}}><CheckIcon /></IconButton>
+           <IconButton className="iconButton" aria-label="edit"  style={{color:"#1769aa", background: "white", border: "solid #1769aa 3px"}} onClick={() => {handleDeleteClick()}}><EditIcon /></IconButton>
+           <IconButton className="iconButton" aria-label="check"  style={{color: todo.isCompleted ? "white" :"#8bc34a", background: todo.isCompleted ? "#8bc34a" : "white", border: "solid #8bc34a 3px"}} onClick={() => {handleCheckClick()}}><CheckIcon /></IconButton>
            </Grid>
            
         </Grid>
